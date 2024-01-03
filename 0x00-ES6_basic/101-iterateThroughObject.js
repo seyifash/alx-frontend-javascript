31fa6e749da80d6933ea65b843eabf02ff0290ba
seyifash <fasugbeoluwaseyi@gmail.com>
2024-01-03 15:33:12 +0000

    iterate

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employeeNames = [];
-
-  // Iterate through the iterator to get each employee name
-  for (const employee of reportWithIterator) {
-    employeeNames.push(employee);
-  }
-
-  // Join the employee names with '|'
-  return employeeNames.join('|');
+	return reportWithIterator.join(' | ');
 }
