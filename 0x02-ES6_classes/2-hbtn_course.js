bf6e1033596f3afe60189e94b9e51c3b8871ad02
seyifash <fasugbeoluwaseyi@gmail.com>
2024-01-04 14:01:07 +0000

    classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -11,7 +11,7 @@ export default class HolbertonCourse {
 
   set name(name) {
     if ((typeof name !== 'string') && !(name instanceof String)) {
-      throw new TypeError('Name  must be a string');
+      throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
