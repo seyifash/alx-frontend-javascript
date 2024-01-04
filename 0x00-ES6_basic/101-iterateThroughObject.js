bf709a222310cf27483eb1011d578f66970cdcc3
seyifash <fasugbeoluwaseyi@gmail.com>
2024-01-04 03:13:28 +0000

    the fix

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,8 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+  const employees = [];
+
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
+  }
+  return employees.join(' | ');
 }
