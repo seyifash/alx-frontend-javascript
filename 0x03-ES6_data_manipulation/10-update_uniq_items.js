1d5ae15f0841dc9baebeed065f03883d5d209c09
seyifash <fasugbeoluwaseyi@gmail.com>
2024-01-08 23:15:19 +0000

    set and map

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  groceriesMap.forEach((quantity, item) => {
+    if (quantity === 1) {
+      groceriesMap.set(item, 100);
+    }
+  });
+}
