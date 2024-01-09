3504becc2dc11fde79e31bd0c9affd4609158baa
seyifash <fasugbeoluwaseyi@gmail.com>
2024-01-09 20:40:40 +0000

    weakmap

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+export const weakMap = new WeakMap();
+
+export const queryAPI = (endpoint) => {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  const queryCount = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, queryCount);
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+};
